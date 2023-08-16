68ffa234b16b0947cec78e9242b98e168bda1625
BigSam-Codes <akindehin2020@gmail.com>
2023-08-16 08:00:32 -0700

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
-
+#ifndef main_h
+#define main_h
 
 	int _putchar(char c);
 	void print_alphabet(void);
@@ -12,4 +13,7 @@
 	void times_table(void);
 	int add(int, int);
 	void print_to_98(int n);
+	void print_times_table(int n);
 
+
+	#endif
